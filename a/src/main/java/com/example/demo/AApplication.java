f7a014f1a6f9142130091d476baabb513c53a2ef
gksrb <gksrb@DESKTOP-DA91TTR>
2019-08-01 16:31:23 +0900

    profile 설정

--- a/src/main/java/com/example/demo/AApplication.java
+++ a/src/main/java/com/example/demo/AApplication.java
@@ -16,11 +16,6 @@ public class AApplication {
 
 	public static void main(String[] args) {
 		
-		String profile = System.getProperty("spring.profiles.active");
-        if(profile == null) {
-            System.setProperty("spring.profiles.active", "linux");
-        }
-
 		SpringApplication.run(AApplication.class, args);
 	}
 
